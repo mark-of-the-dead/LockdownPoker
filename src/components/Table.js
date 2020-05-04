6c2c65121651ad37af8ac0791a27d19eb93e72a5
Mark Wallace <markwallace@UX-MacBook-Pro.local>
2020-05-04 12:49:38 +0100

    added some styling to the community cards

--- src/components/Table.js
+++ src/components/Table.js
@@ -5,10 +5,10 @@ class Table extends React.Component {
   render(){
 
     const flop = this.props.cards['flop'] ? Object.keys(this.props.cards['flop']).map(
-      key => this.props.cards['flop'] ? <li key={key} className="table-card">{this.props.cards['flop'][key]}</li> : ''
+      key => this.props.cards['flop'] ? <li key={key} className={`table-card ${this.props.cards['flop'][key]}`}>{this.props.cards['flop'][key]}</li> : ''
     ) : '';
-    const turn = this.props.cards['turn'] ? <li className="table-card">{this.props.cards['turn']}</li> : '';
-    const river = this.props.cards['river'] ? <li className="table-card">{this.props.cards['river']}</li> : '';
+    const turn = this.props.cards['turn'] ? <li className={`table-card ${this.props.cards['turn']}`}>{this.props.cards['turn']}</li> : '';
+    const river = this.props.cards['river'] ? <li className={`table-card ${this.props.cards['river']}`}>{this.props.cards['river']}</li> : '';
 
 
     return (
